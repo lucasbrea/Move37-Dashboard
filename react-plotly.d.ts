d9002aedf8cc37562f8d3ab3e0593df6c3d2fad7
Lucas Brea <lucasjbrea@gmail.com>
2025-06-03 11:28:07 -0300

    added plotly layout

--- react-plotly.d.ts
+++ react-plotly.d.ts
@@ -1,1 +1,18 @@
-declare module 'react-plotly.js';
+declare module 'react-plotly.js' {
+    import * as React from 'react';
+    import { Layout, Data } from 'plotly.js';
+  
+    export interface PlotParams {
+      data: Data[];
+      layout?: Partial<Layout>;
+      config?: object;
+      style?: React.CSSProperties;
+      className?: string;
+      useResizeHandler?: boolean;
+      onInitialized?: (figure: { data: Data[]; layout: Partial<Layout> }) => void;
+      onUpdate?: (figure: { data: Data[]; layout: Partial<Layout> }) => void;
+    }
+  
+    const Plot: React.FC<PlotParams>;
+    export default Plot;
+  }
